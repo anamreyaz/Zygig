712be2d4960490f446af63dcc8276187549ddf97
anamreyaz <116513594+anamreyaz@users.noreply.github.com>
2024-11-01 15:58:29 +0530

    Add files via upload

---
+++ src/constants/index.jsx
@@ -0,0 +1,173 @@
+import { AppWindow, AppWindowIcon, AppWindowMac, BotMessageSquare, Laptop, Paperclip, PenSquare, Phone } from "lucide-react";
+import { BatteryCharging } from "lucide-react";
+import { Fingerprint } from "lucide-react";
+import { ShieldHalf } from "lucide-react";
+import { PlugZap } from "lucide-react";
+import { GlobeLock } from "lucide-react";
+
+import user1 from "../assets/profile-pictures/user1.jpg";
+import user2 from "../assets/profile-pictures/user2.jpg";
+import user3 from "../assets/profile-pictures/user3.jpg";
+import user4 from "../assets/profile-pictures/user4.jpg";
+import user5 from "../assets/profile-pictures/user5.jpg";
+import user6 from "../assets/profile-pictures/user6.jpg";
+import App from "../App";
+
+export const navItems = [
+  { label: "Features", href: "#" },
+  { label: "Workflow", href: "#" },
+  { label: "Pricing", href: "#" },
+  { label: "Testimonials", href: "#" },
+];
+
+export const testimonials = [
+  {
+    user: "John Doe",
+    company: "Stellar Solutions",
+    image: user1,
+    text: "I am extremely satisfied with the services provided. The team was responsive, professional, and delivered results beyond my expectations.",
+  },
+  {
+    user: "Jane Smith",
+    company: "Blue Horizon Technologies",
+    image: user2,
+    text: "I couldn't be happier with the outcome of our project. The team's creativity and problem-solving skills were instrumental in bringing our vision to life",
+  },
+  {
+    user: "David Johnson",
+    company: "Quantum Innovations",
+    image: user3,
+    text: "Working with this company was a pleasure. Their attention to detail and commitment to excellence are commendable. I would highly recommend them to anyone looking for top-notch service.",
+  },
+  {
+    user: "Ronee Brown",
+    company: "Fusion Dynamics",
+    image: user4,
+    text: "Working with the team at XYZ Company was a game-changer for our project. Their attention to detail and innovative solutions helped us achieve our goals faster than we thought possible. We are grateful for their expertise and professionalism!",
+  },
+  {
+    user: "Michael Wilson",
+    company: "Visionary Creations",
+    image: user5,
+    text: "I am amazed by the level of professionalism and dedication shown by the team. They were able to exceed our expectations and deliver outstanding results.",
+  },
+  {
+    user: "Emily Davis",
+    company: "Synergy Systems",
+    image: user6,
+    text: "The team went above and beyond to ensure our project was a success. Their expertise and dedication are unmatched. I look forward to working with them again in the future.",
+  },
+];
+
+export const features = [
+  {
+    icon: <Laptop />,
+    text: "Web Development",
+    description:
+      "We create modern, responsive websites tailored to your brand and business needs.",
+  },
+  {
+    icon: <AppWindow />,
+    text: "App Development",
+    description:
+      "We build custom, high-performance mobile apps for iOS, Android, and cross-platform.",
+  },
+  {
+    icon: <BotMessageSquare />,
+    text: "Machine Learning Models",
+    description:
+      "We develop custom machine learning models for automation, prediction, and decision-making.",
+  },
+  {
+    icon: <PenSquare />,
+    text: "Research Papers",
+    description:
+      "We offer professional writing, editing, and formatting services for research papers.",
+  },
+  {
+    icon: <GlobeLock />,
+    text: " UI/UX Design",
+    description:
+      "We design intuitive, user-friendly interfaces for apps and websites.",
+  },
+];
+
+export const checklistItems = [
+  {
+    title: "Tailored Solutions",
+    description:
+      "We deliver customized services that align perfectly with your unique business needs.",
+  },
+  {
+    title: "Affordable Pricing",
+    description:
+      "High-quality solutions at competitive prices make professional services accessible to all.",
+  },
+  {
+    title: "Expert Team",
+    description:
+      "Our skilled team brings expertise across app development, web development, research papers, machine learning models and UI/UX design",
+  },
+  {
+    title: "Commitment to Quality",
+    description:
+      "We prioritize reliable, high-standard solutions to drive real results for your business.",
+  },
+];
+
+export const pricingOptions = [
+  {
+    title: "Minimal",
+    price: "2000",
+    features: [
+      "Private board sharing",
+      "5 Gb Storage",
+      "Web Analytics",
+      "Private Mode",
+    ],
+  },
+  {
+    title: "Moderate",
+    price: "8000",
+    features: [
+      "Private board sharing",
+      "10 Gb Storage",
+      "Web Analytics (Advance)",
+      "Private Mode",
+    ],
+  },
+  {
+    title: "Advance",
+    price: "18000",
+    features: [
+      "Private board sharing",
+      "Unlimited Storage",
+      "High Performance Network",
+      "Private Mode",
+    ],
+  },
+];
+
+export const resourcesLinks = [
+  { href: "#", text: "Getting Started" },
+  { href: "#", text: "Documentation" },
+  { href: "#", text: "Tutorials" },
+  { href: "#", text: "API Reference" },
+  { href: "#", text: "Community Forums" },
+];
+
+export const platformLinks = [
+  { href: "#", text: "Features" },
+  { href: "#", text: "Supported Devices" },
+  { href: "#", text: "System Requirements" },
+  { href: "#", text: "Downloads" },
+  { href: "#", text: "Release Notes" },
+];
+
+export const communityLinks = [
+  { href: "#", text: "Events" },
+  { href: "#", text: "Meetups" },
+  { href: "#", text: "Conferences" },
+  { href: "#", text: "Hackathons" },
+  { href: "#", text: "Jobs" },
+];
